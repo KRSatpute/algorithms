bebb700a1fe1dd04d027c45f452127347deb3c10
krsatpute <krsatpute@github.com>
2017-11-13 10:48:26 +0530

    Fixed the recursion break condition

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -44,47 +44,52 @@ def recursive_search(element_list_sorted, element):
     It returns location of x in given array arr if present,
     else returns -1
     """
+    length_list = len(element_list_sorted)
     index_start = 0
-    index_last = len(element_list_sorted) - 1
+    index_last = length_list - 1
 
     index_mid = index_start + (index_last - index_start) / 2
 
-    if index_mid == 0 and element_list_sorted[index_mid] != element:
-        return -1
-
-    if element_list_sorted[index_mid] == element:
-        # Check if element is present at mid
-        return index_mid
-    elif element_list_sorted[index_mid] < element:
-        # If element is greater, ignore left half
-        # Beware we are shifting the start position
-        # in the upcoming recursive call
-
-        index = recursive_search(
-            element_list_sorted[index_mid + 1:], element
-        )
-
-        return (index_mid + 1) + index if index != -1 else -1
+    if length_list == 2:
+        if element_list_sorted[0] == element:
+            return 0
+        elif element_list_sorted[1] == element:
+            return 1
+        else:
+            return -1
     else:
-        # If element is smaller, ignore right half
-        return recursive_search(
-            element_list_sorted[: index_mid], element
-        )
+        if element_list_sorted[index_mid] == element:
+            # Check if element is present at mid
+            return index_mid
+        elif element_list_sorted[index_mid] < element:
+            # If element is greater, ignore left half
+            # Beware we are shifting the start position
+            # in the upcoming recursive call
+
+            index = recursive_search(
+                element_list_sorted[index_mid + 1:], element
+            )
+
+            return (index_mid + 1) + index if index != -1 else -1
+        else:
+            # If element is smaller, ignore right half
+            return recursive_search(
+                element_list_sorted[: index_mid], element
+            )
 
     # If we reach here, then the element was not present
     return -1
 
 # Running the code
-"""
-LIST_OF_ELEMS = [2, 3, 4, 10, 40, 44, 45, 89, 101, 112, 178, 205]
 
+LIST_OF_ELEMS = [2, 3, 4, 10, 40, 44, 45, 89, 101, 112, 178, 205]
+"""
 print iterative_search(LIST_OF_ELEMS, 101)  # will print 8
 print iterative_search(LIST_OF_ELEMS, 300)  # will print -1
 print iterative_search(LIST_OF_ELEMS, 1)  # will print -1
 print iterative_search(LIST_OF_ELEMS, 2)  # will print 0
-
 print recursive_search(LIST_OF_ELEMS, 101)  # will print 8
 print recursive_search(LIST_OF_ELEMS, 300)  # will print -1
 print recursive_search(LIST_OF_ELEMS, 1)  # will print -1
-print recursive_search(LIST_OF_ELEMS, 2)  # will print 0
+print recursive_search(LIST_OF_ELEMS[0:2], 3)  # will print 1
 """
